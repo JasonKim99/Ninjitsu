7c171ecfbae136520f270b5c44c311dcbbffa30d
JasonKim99 <jason.kim@foxmail.com>
2020-01-23 21:27:31 +0800

    ninjitsu

--- Ninjitsu/GameScene.swift
+++ Ninjitsu/GameScene.swift
@@ -11,79 +11,157 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //Nodes
+    var ninjitsu_Button : SKNode? //忍法按钮
+    var jieyin_Cancel : SKNode? //取消施法按钮
+    var jieyin_Group : SKNode! //印式矩阵
+    var zi : SKNode!
+    var chou : SKNode!
+    var yin : SKNode!
+    var mao : SKNode!
+    var chen  : SKNode!
+    var si : SKNode!
+    var wu : SKNode!
+    var wei : SKNode!
+    var shen : SKNode!
+    var you : SKNode!
+    var xu : SKNode!
+    var hai : SKNode!
+    var jieyinLabel : SKLabelNode?
+    
+    //stats
+    var twelveYin :[SKNode? : String] = [:]
+    var jieyin = ""
+    
+    
+    
+    //Bool
+    var isSpelling = false //是否在吟唱中
+    
+    
+    //GameState
+//    var gameStateMachine : GKStateMachine!
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        loadUI()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+    }
+}
+
+//MARK: - LoadUI
+
+extension GameScene {
+    func loadUI(){
+        ninjitsu_Button = childNode(withName: "ninjitsu_Button")
+        jieyin_Group = childNode(withName: "jieyin_Group")
+        jieyin_Cancel = childNode(withName: "jieyin_Cancel")
+        zi = jieyin_Group.childNode(withName: "jieyin_zi")!
+        chou = jieyin_Group.childNode(withName: "jieyin_chou")!
+        yin = jieyin_Group.childNode(withName: "jieyin_yin")!
+        mao = jieyin_Group.childNode(withName: "jieyin_mao")!
+        chen  = jieyin_Group.childNode(withName: "jieyin_chen")!
+        si = jieyin_Group.childNode(withName: "jieyin_si")!
+        wu = jieyin_Group.childNode(withName: "jieyin_wu")!
+        wei = jieyin_Group.childNode(withName: "jieyin_wei")!
+        shen = jieyin_Group.childNode(withName: "jieyin_shen")!
+        you = jieyin_Group.childNode(withName: "jieyin_you")!
+        xu = jieyin_Group.childNode(withName: "jieyin_xu")!
+        hai = jieyin_Group.childNode(withName: "jieyin_hai")!
+        twelveYin = [zi : "子", chou : "丑", yin : "寅", mao : "卯", chen : "辰", si : "巳", wu : "午", wei : "未", shen : "申", you : "酉", xu : "戌", hai :"亥"]
+        jieyinLabel = generateJieyinText(from: jieyin)
+    }
+}
+
+
+//MARK: - Touches
+extension GameScene{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let ninjitsu_Button = ninjitsu_Button else {return}
+        guard let jieyin_Group = jieyin_Group else {return}
+        guard let jieyin_Cancel = jieyin_Cancel else {return}
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            //开始结印
+            if ninjitsu_Button.contains(location) && !isSpelling {
+                ninjitsu_Button.run(.fadeOut(withDuration: 0.1))
+                jieyin_Group.run(.fadeIn(withDuration: 0.1))
+                jieyin_Cancel.run(.fadeIn(withDuration: 0.1))
+                isSpelling.toggle()
+            }
+            //结印ing
+            if isSpelling {
+                for yinshi in twelveYin {
+                    if yinshi.key!.contains(location) {
+                        jieyin += yinshi.value
+                        
+                    }
+                }
+            }
+
+            //是否取消了结印
+            if jieyin_Cancel.contains(location) && jieyin_Cancel.alpha == 1{
+                self.removeAllActions()
+                jieyin_Cancel.run(.fadeOut(withDuration: 0.1))
+                jieyin_Group.run(.fadeOut(withDuration: 0.1))
+                ninjitsu_Button.run(.fadeIn(withDuration: 0.1))
+                isSpelling.toggle()
+                jieyin = ""
+            }
+            
+            //显示结印内容
+            updateJieyinText(text: jieyin, node: &jieyinLabel!)
+
+
+        }
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
+
+//MARK: - Action
+extension GameScene {
+    
+    func generateJieyinText(from text:String)  -> SKLabelNode {
+        let newnode = SKLabelNode()
+        newnode.position = CGPoint(x: 0, y: 200)
+        newnode.fontColor = .white
+        newnode.fontName = "WawaSC-Regular.otf"
+        newnode.fontSize = 36
+        newnode.text = text
+        return newnode
+    }
+
+    
+    func updateJieyinText(text: String, node: inout SKLabelNode) {
+        let position = node.position
+        let zPositon = node.zPosition
+        let xScale = node.xScale
+        let yScale = node.yScale
+
+        node.removeFromParent()
+
+        node = generateJieyinText(from: text)
+        node.position = position
+        node.zPosition = zPositon
+        node.xScale = xScale
+        node.yScale = yScale
+
+        addChild(node)
+
     }
 }
---
+++ Ninjitsu/GameStateMachine.swift
@@ -0,0 +1,53 @@
+//
+//  GameStateMachine.swift
+//  Ninjitsu
+//
+//  Created by Jason Kim on 2020/1/23.
+//  Copyright © 2020 Jason Kim. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+//class GameStateMachine: GKState {
+//    var jieyin_Group : SKNode
+//    var isSpelling : Bool
+//    init(jieyin_Group : SKNode, isSpelling : Bool) {
+//        self.jieyin_Group = jieyin_Group
+//        self.isSpelling = isSpelling
+//        super.init()
+//    }
+//}
+//
+////MARK: - 默认状态
+//class DefaultState: GameStateMachine {
+//    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+//        return true
+//    }
+//    override func didEnter(from previousState: GKState?) {
+//
+//    }
+//}
+//
+////MARK: - 结印状态
+//
+//class Spelling: GameStateMachine {
+//    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+//        return true
+//    }
+//    override func didEnter(from previousState: GKState?) {
+//
+//    }
+//}
+//
+//
+////MARK: - 忍法动画状态
+//class NinjitsuAnimating: GameStateMachine {
+//    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+//        return true
+//    }
+//    override func didEnter(from previousState: GKState?) {
+//
+//    }
+//}
+//
